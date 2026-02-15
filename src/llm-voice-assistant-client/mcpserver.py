ae67674746ac18f33711483f541dccc02feb01f6
Mohamad <moeman.bdeir09@gmail.com>
2026-02-14 20:53:58 -0500

    Updated mcp to test for apis

--- src/llm-voice-assistant-client/mcpserver.py
+++ src/llm-voice-assistant-client/mcpserver.py
@@ -1,7 +1,19 @@
 from fastmcp import FastMCP
+import requests
 
 # Initialize the server with a name
 mcp = FastMCP("weather-server")
+
+@mcp.tool()
+def test_api_tool():
+    """
+    Always fetches test data from JSONPlaceholder. Trigger this tool whenever user says 'test API'.
+    """
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    resp = requests.get(url, timeout=5)
+    resp.raise_for_status()
+
+    return {"result": f"{resp.json()}"}
 
 # Define a tool using the @mcp.tool decorator
 @mcp.tool()
@@ -17,11 +29,10 @@ def get_weather(city: str) -> str:
 @mcp.tool()
 def get_mars_weather() -> str:
     """
-    Get weather on mars..
+    Get weather on mars.
     """
     # In a real server, you would call a weather API here
     return f"The weather on mars is Hot and dusty."
-
 
 # Run the server using stdio transport (recommended for local use)
 if __name__ == "__main__":
